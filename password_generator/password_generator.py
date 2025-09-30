26faba276497724c9c2786f54969d852fdf93944
Mantra Patil <mantrapatil2007@gmail.com>
2025-09-30 22:39:22 +0530

    Removing unnecessary Code

--- password_generator/password_generator.py
+++ password_generator/password_generator.py
@@ -48,17 +48,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-# This script generates a secure password based on user-defined length.
-# It ensures the password contains at least one uppercase letter, one lowercase letter,
-# one digit, and one special character for enhanced security.
-# It also shuffles the characters to avoid predictable patterns.
-# The user is prompted to enter the desired length of the password, with a minimum requirement of 6 characters.
-# The generated password is then displayed to the user.
 
-''' 
-Example usage:
-$ python password_generator.py
-Secure Password Generator
-Enter desired password length (minimum 6): 12
-Generated password: A1b!cD2e@F3g
-'''
